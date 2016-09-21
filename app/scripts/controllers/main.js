8c9fcef40b46509cc05b91e1cdfe578d8966383c
Kenrelya <soso_9.4@live.fr>
2016-09-21 11:21:08 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seekstreamFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the seekstreamFrontApp
+ */
+angular.module('seekstreamFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
